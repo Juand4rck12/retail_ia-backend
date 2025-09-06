7bce13e77e402f1deae355d27a6e6a485e2ae9a0
Juan_Diego <juan3312orrego@gmail.com>
2025-09-05 21:16:21 -0500

    Configuración inicial del proyecto en Spring Boot
    Creación del proyecto y conexión a BD

---
+++ retail-backend/src/test/java/retail/ai/project/retailbackend/RetailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package retail.ai.project.retailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
